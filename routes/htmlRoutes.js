b3f8041bad3c2e28ab6d2d8565d6b71089f832c7
UnDuhDuhSea <tafimba@gmail.com>
2021-04-18 10:29:40 -0700

    Updating file structure and adding initial server.js functionality

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,19 @@
+//DEPENDENCIES
+const router = require("express").Router();
+const path = require("path");
+
+//ROUTING
+
+// module.exports = (app) => {
+// => HTML GET Requests
+// Below code handles when users "visit" a page.
+// In each of the below cases the user is shown an HTML page of content
+router.get("/notes", function (req, res) {
+  res.json(path.join(__dirname, "..", "public", "notes.html"));
+});
+// If no matching route is found default to home
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+// };
+module.exports = router;
